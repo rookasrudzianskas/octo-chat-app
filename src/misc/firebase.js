94f2715788ce38af2a9fac0a602c590bbb452922
Rokas <rookas.rudzenskas@gmail.com>
2021-04-13 07:26:51 +0300

    Rokas

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+
+const config = {
+    apiKey: "AIzaSyBxbNSunu5PHrv1rNlgMjDNGXm94lGEzyw",
+    authDomain: "octo-chat-app.firebaseapp.com",
+    projectId: "octo-chat-app",
+    storageBucket: "octo-chat-app.appspot.com",
+    messagingSenderId: "586485662100",
+    appId: "1:586485662100:web:142ff93eb643ab64220f81"
+};
+
+const app = firebase.initializeApp(config);
