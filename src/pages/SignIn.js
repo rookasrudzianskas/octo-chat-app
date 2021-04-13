f5ae6686c346dc4e4ac44691eb7d411dc5248626
Rokas <rookas.rudzenskas@gmail.com>
2021-04-13 08:32:08 +0300

    Rokas

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import 'rsuite/dist/styles/rsuite-default.css';
+import { Container, Grid, Row, Panel, Col, Button, Icon } from 'rsuite';
 
 const SignIn = () => {
     return (
-        <div>
-            signIn
-        </div>
-    )
+        <Container>
+            <Grid>
+                <Row>
+                    <Col xs={24} md={12} mdOffset={6}>
+                        <Panel>
+                            <div className="text-center">
+                                <h2>Welcome to Chat</h2>
+                                <p>Progressive chat platform for Developers</p>
+                            </div>
+
+                            <div>
+                                <Button block color="blue">
+                                    <Icon icon="facebook" /> Continue with Facebook
+                                </Button>
+                                <Button block color="green">
+                                    <Icon icon="google" /> Continue with Google
+                                </Button>
+                            </div>
+                        </Panel>
+                    </Col>
+                </Row>
+            </Grid>
+        </Container>
+    );
 };
 
 export default SignIn;
